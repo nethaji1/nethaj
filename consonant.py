f40326f69dd2fb8937dda9c3008c37fbdc1fd110
nethaji1 <42028426+nethaji1@users.noreply.github.com>
2018-08-07 12:30:44 +0530

    Create consonant.py

---
+++ consonant.py
@@ -0,0 +1,9 @@
+2 = input("Input a letter of the alphabet: ")
+
+if 2 in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % 2)
+else if 2 == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s is a consonant." % 2) 
+	
